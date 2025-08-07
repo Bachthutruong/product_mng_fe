f838587358b69e9ff3a47d9c81a1dc9c91b36ab1
bachvd <vuduybachvp@gmail.com>
2025-08-07 10:35:33 +0700

    fix order

--- vite.config.ts
+++ vite.config.ts
@@ -13,8 +13,8 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        // target: 'https://product-mng-be.onrender.com',
-        target: 'http://localhost:5002',
+        target: 'https://product-mng-be.onrender.com',
+            // target: 'http://localhost:5002',
         changeOrigin: true,
         secure: false,
       },
